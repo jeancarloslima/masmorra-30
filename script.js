b00d91f2821c5ccabadebd0e5826f44c0b37caa8
Jean Carlos Oliveira Lima <jcoliveiralima19@gmail.com>
2025-07-11 15:18:47 -0300

    Adicionando sistema básico do menu

--- script.js
+++ script.js
@@ -5,6 +5,21 @@ const inputNome = document.getElementById("nome");
 const botaoAvancaTela1 = document.getElementById("avanca-tela1");
 const botoesEscolherClasse = document.querySelectorAll(".tela-escolher-classe-botao");
 const botaoAvancaTela2 = document.getElementById("avanca-tela2");
+const nomeJogadorCampo = document.getElementById("nome-jogador");
+const rodapeMenu = document.getElementById("rodape-jogo-menu");
+const rodapeMenuCombate = document.getElementById("rodape-jogo-combate");
+const rodapeMenuFeiticos = document.getElementById("rodape-jogo-feiticos");
+const rodapeMenuBolsa = document.getElementById("rodape-jogo-bolsa");
+const botaoAbreCombate = document.getElementById("btn-abre-combate");
+const botaoAbreFeiticos = document.getElementById("btn-abre-feiticos");
+const botaoAbreBolsa = document.getElementById("btn-abre-bolsa");
+const botaoFoge = document.getElementById("btn-foge");
+const botoesVoltar = document.querySelectorAll(".btn-voltar");
+let dias;
+let inimigos;
+let jogador;
+let nome;
+let classe;
 
 inputNome.addEventListener("input", () => {
     if (inputNome.value !== "") {
@@ -17,6 +32,8 @@ inputNome.addEventListener("input", () => {
 botaoAvancaTela1.addEventListener("click", () => {
     telaEscolherNome.style.display = "none";
     telaEscolherClasse.style.display = "flex";
+
+    nome = inputNome.value.toLowerCase();
 });
 
 botoesEscolherClasse.forEach((botao) => {
@@ -27,6 +44,8 @@ botoesEscolherClasse.forEach((botao) => {
 
         botao.classList.add("selecionado");
 
+        classe = botao.textContent.toLowerCase();
+
         botaoAvancaTela2.style.pointerEvents = "all";
     })
 });
@@ -34,4 +53,42 @@ botoesEscolherClasse.forEach((botao) => {
 botaoAvancaTela2.addEventListener("click", () => {
     telaEscolherClasse.style.display = "none";
     telaJogo.style.display = "flex";
+
+    constroiJogador();
 });
+
+botaoAbreCombate.addEventListener("click", () => {
+    rodapeMenu.style.display = "none";
+    rodapeMenuCombate.style.display = "flex";
+})
+
+botaoAbreFeiticos.addEventListener("click", () => {
+    rodapeMenu.style.display = "none";
+    rodapeMenuFeiticos.style.display = "flex";
+})
+
+botaoAbreBolsa.addEventListener("click", () => {
+    rodapeMenu.style.display = "none";
+    rodapeMenuBolsa.style.display = "flex";
+})
+
+botoesVoltar.forEach((botao) => {
+    botao.addEventListener("click", () => {
+        botao.parentElement.style.display = "none";
+        rodapeMenu.style.display = "flex";
+    })
+})
+
+function constroiJogador() {
+    jogador = {
+        nome,
+        classe,
+        vida: 5,
+        forca: 5,
+        magia: 5,
+        velocidade: 5,
+        armadura: 5
+    }
+    
+    nomeJogadorCampo.textContent = nome;
+}
